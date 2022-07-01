c5967be6306bdcbcd359d7fa12943d6154914b0f
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-07-01 10:57:06 -0500

    first commit

---
+++ Variables/Program.cs
@@ -0,0 +1,10 @@
+﻿string name = "Alex Coleman";
+int dayOfMonth = 1;
+char singleLetter = 'A';
+bool inClass = true;
+double aDouble = 1.234;
+decimal decimalNumber = 2.345m;
+
+
+Console.WriteLine($"My name is {name} it is the {dayOfMonth}st of the month. I am currently in class: {inClass}." +
+    $" Here is a random double {aDouble}. here is a random decimal {decimalNumber}.");
